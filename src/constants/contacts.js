989fdae64356c464c22d83250e6539da9cb41ba5
Miosha Tala <miosha.tala@gmail.com>
2025-05-25 16:57:14 +0300

    done

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,6 @@
-export const PATH_DB =
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const thisModulePath = path.dirname(fileURLToPath(import.meta.url));
+
+export const PATH_DB = path.resolve(thisModulePath, '../db/db.json');
